a9f3198f418025d7be7e3a9c0a588f8b80db6f1f
Naveen Saggam <thenaveensaggam@gmail.com>
2019-03-28 19:27:50 +0530

    added

---
+++ 6PM_BATCH/11_JS_DOM_Events/11_script.js
@@ -0,0 +1,21 @@
+// Good Morning
+let sayGoodMorning = () => {
+    let h2Tag = document.querySelector('#msg');
+    h2Tag.textContent = 'Good Morning';
+    h2Tag.style.backgroundColor = 'teal';
+    h2Tag.style.padding = '10px';
+    h2Tag.style.color = 'white';
+    h2Tag.style.boxShadow = '0 0 10px black';
+};
+
+
+// Good Afternoon
+let sayGoodAfternoon = () => {
+    let h2Tag = document.querySelector('#msg');
+    h2Tag.textContent = 'Good Afternoon';
+    h2Tag.style.backgroundColor = 'orangered';
+    h2Tag.style.padding = '10px';
+    h2Tag.style.color = 'white';
+    h2Tag.style.boxShadow = '0 0 10px black';
+};
+
